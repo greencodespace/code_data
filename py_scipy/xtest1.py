a024947cfe541c91407ed65777e0241b2067d6e0
Rocker <40746581+greencodespace@users.noreply.github.com>
2019-08-13 14:04:35 +0900

    Add files via upload

---
+++ py_scipy/xtest1.py
@@ -0,0 +1,39 @@
+
+
+# -*- coding : utf-8 -*-
+
+#-----------------------------------------------
+#  file_name : xtest1.py                                  
+#  theme :  scipy_1st_test_code
+#  start :
+#  last_uodate :
+#  kw    :                                       
+#  from  :                                       
+#  ref   :                                       
+#------------------------------------------------
+
+from numpy import *
+from scipy import *
+from pylab import *
+from scipy import ndimage
+
+
+print('hello_peace_world')
+print('%10.5f' % 38)
+
+def myplot(a,b,c):
+
+    fig=plt.figure(facecolor='w');
+    x=arange(a,b,c);
+    plot(x,sin(x));
+    show();
+    del x;
+
+if __name__ == "__main__":
+
+    myplot(0,10,0.1);
+
+
+
+
+# c
